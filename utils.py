ba658c26b9637b32db926bf5b1b7f3ba4b13dce5
Yohsuke Fukai <ysk@yfukai.net>
2020-06-05 19:47:35 +0900

    resetted to working commit

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import treelib
+
 def make_random_colormap(cmap,number,alpha):
     colormap=np.array(cmap(np.linspace(0.0,.9,number-1)) * 255).astype(np.uint8)
     colormap_random=colormap.copy()
@@ -21,3 +23,28 @@ def get_testdata(size=(10,1000,500)):
     for j, i in enumerate(unique_indices):
         if i != 0: mask2[mask == i] = j
     return data,mask2
+
+def tree_to_segments(tree,node):
+    segment=[node.identifier]
+    while True:
+        children=tree.children(node.identifier)
+        if len(children)==0:
+            return segment,None
+        elif len(children)==1:
+            node=children[0]
+            segment.append(node.identifier)
+        else:
+            return segment, [tree_to_segments(tree,c) for c in children]
+        
+def create_nodes(tree,segments,parent):
+    for s in segments:
+        k=s[0][0]
+        tree.create_node(k,k,parent=parent)
+        if not s[1] is None:
+            create_nodes(tree,s[1],k)
+            
+def tree_to_segment_tree(tree,node):
+    segments=tree_to_segments(tree,node)
+    segment_tree=treelib.Tree()
+    create_nodes(segment_tree,[segments],None)
+    return segment_tree
